1efe32918c53f0edd6a10fa7301836ac3db88d6b
Mariana Carvalho <marianapicolic@gmail.com>
2021-06-22 10:57:28 -0300

    exs poo

---
+++ src/cursoNelioAlves/cap8/application/Program.java
@@ -0,0 +1,42 @@
+package cursoNelioAlves.cap8.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import cursoNelioAlves.cap8.Triangle;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
+		
+		System.out.println("Enter the measures of triangle x: ");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		System.out.println("Enter the measures of triangle y: ");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		
+		double areaX = x.area();
+		double areaY = y.area();
+		
+		System.out.printf("Tiangle X area: %.4f%n", areaX);
+		System.out.printf("Tiangle Y area: %.4f%n", areaY);
+		
+		if (areaX > areaY) {
+			System.out.println("Larger area: X");
+		}
+		else
+			System.out.println("Larger area: Y");
+		
+	}
+
+}
---
+++ src/cursoNelioAlves/cap8/application/ProgramEx2.java
@@ -0,0 +1,44 @@
+package cursoNelioAlves.cap8.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import cursoNelioAlves.cap8.ProductEx2;
+
+public class ProgramEx2 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		ProductEx2 product = new ProductEx2();
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		product.quantity = sc.nextInt();
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+
+	}
+
+}
